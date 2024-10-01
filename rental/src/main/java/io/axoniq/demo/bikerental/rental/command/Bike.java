f07877dc773d5b71d75a7765b03135f67f58ddc3
Bjørn Grønbæk <bjg@usercentrics.com>
2024-10-01 10:29:02 +0000

    Fixed request command handler

--- rental/src/main/java/io/axoniq/demo/bikerental/rental/command/Bike.java
+++ rental/src/main/java/io/axoniq/demo/bikerental/rental/command/Bike.java
@@ -1,5 +1,13 @@
 package io.axoniq.demo.bikerental.rental.command;
 
+import java.util.UUID;
+
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateCreationPolicy;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.modelling.command.CreationPolicy;
 import org.axonframework.spring.stereotype.Aggregate;
 import io.axoniq.demo.bikerental.coreapi.rental.*;
 
@@ -8,6 +16,8 @@ public class Bike {
 
     @AggregateIdentifier
     private String bikeId;
+    private String rentalReference;
+    private String renter;
 
     @CommandHandler
     @CreationPolicy(AggregateCreationPolicy.ALWAYS)
@@ -15,4 +25,26 @@ public class Bike {
         AggregateLifecycle.apply(new BikeRegisteredEvent(
             command.bikeId(), command.bikeType(), command.location()));
     }
+
+    @CommandHandler
+    public String handle(RequestBikeCommand command){
+        String rentalReference = UUID.randomUUID().toString();
+        AggregateLifecycle.apply(new BikeRequestedEvent(
+            command.bikeId(), command.renter(),rentalReference));
+        return rentalReference;
+    }
+
+    @EventSourcingHandler
+    public void handleBikeRegisteredEvent(BikeRegisteredEvent event){
+        this.bikeId = event.bikeId();
+    }
+
+    @EventSourcingHandler
+    public void handleBikeRequestedEvent(BikeRequestedEvent event){
+        this.rentalReference = event.rentalReference();
+        this.renter = event.renter();
+    }
+
+
+
 }
