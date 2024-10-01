e759520bfe9731b39a717167789247e778705dba
Bjørn Grønbæk <github@gronbaek.net>
2024-10-01 09:48:58 +0000

    First

--- rental/src/main/java/io/axoniq/demo/bikerental/rental/command/Bike.java
+++ rental/src/main/java/io/axoniq/demo/bikerental/rental/command/Bike.java
@@ -1,8 +1,18 @@
 package io.axoniq.demo.bikerental.rental.command;
 
 import org.axonframework.spring.stereotype.Aggregate;
+import io.axoniq.demo.bikerental.coreapi.rental.*;
 
 @Aggregate
 public class Bike {
 
+    @AggregateIdentifier
+    private String bikeId;
+
+    @CommandHandler
+    @CreationPolicy(AggregateCreationPolicy.ALWAYS)
+    public void handle(RegisterBikeCommand command){
+        AggregateLifecycle.apply(new BikeRegisteredEvent(
+            command.bikeId(), command.bikeType(), command.location()));
+    }
 }
